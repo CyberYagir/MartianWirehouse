9d1f558702d5cc13ca0390c591bde49b1abce750
Yagir <76903670+CyberYagir@users.noreply.github.com>
2022-10-31 01:50:51 +0300

    Initial commit

---
+++ Source/MartianWirehouse/MartianWirehouseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MartianWirehouseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MARTIANWIREHOUSE_API AMartianWirehouseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
